04c12874c0afc0ebd0ce8c742822d81d965ea5b1
Purushottam Kumar <purushottam2fx@gmail.com>
2025-09-29 19:51:20 +0530

    i done api patch method

--- merge_api_with_database.js
+++ merge_api_with_database.js
@@ -30,6 +30,17 @@ app.post("/create_user",(userRequest,userResponse)=>{
     })
 })
 
+app.patch("/patch_user/:id",(userRequest,userResponse)=>{
+    const mysqlPatch="UPDATE student SET name = ? WHERE id = ?";
+    const id=userRequest.params.id;
+    const name=userRequest.body.name;
+    db.query(mysqlPatch,[name,id],(error,result)=>{
+        if(error) return userResponse.status(500).json({"message":"server error","data":[]});
+        if(result.affectedRows===0) return userResponse.status(404).json({"message":"user not found","data":[]});
+        userResponse.status(200).json({"message":"user data updated","data":{id,name}});
+    })
+})
+
 app.listen(5213,(error)=>{
     if(error) return console.log("error:-" + error.message);
     console.log("Purushottam Kumar");
